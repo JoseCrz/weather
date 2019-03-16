094ccf7958e831aa0dff5dc3af42bcae1c2802e5
José Cuevas <josecueram@gmail.com>
2019-03-15 21:26:08 -0600

    Added basic error handling

--- app.js
+++ app.js
@@ -4,20 +4,39 @@ const chalk = require('chalk')
 const url = 'https://api.darksky.net/forecast/b570cea920d6af533ac24dbe957983e3/37.8267,-122.4233?units=si'
 
 request({url: url, json: true}, (error, response) => {
-    const currentWeather = response.body.currently
-    //console.log(currentWeather)
-    console.log(response.body.daily.data[0].summary)
-    console.log(`It is currently ${currentWeather.temperature} degrees outside. There's a ${currentWeather.precipProbability}% chance of rain `)
+    if (error) {
+        console.log(chalk.bgRed('Could not connect with the server'))
+
+    } else if (response.body.error) {
+        console.log(chalk.bgYellow(response.body.error))
+        
+    } else {
+        const currentWeather = response.body.currently
+        //console.log(currentWeather)
+        console.log(response.body.daily.data[0].summary)
+        console.log(`It is currently ${currentWeather.temperature} degrees outside. There's a ${currentWeather.precipProbability}% chance of rain `)
+    }
+
 })
 
 const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYnlrbzAxMyIsImEiOiJjanRhdnJxOWUwM2l6NDRxamN1cDJqdmQ1In0.OKOEbro8Zm1ZQjfLJo_kLw'
 request({url: url2, json: true}, (error, response) => {
-    //console.log(response.body)
-    const place = response.body.features[0]
-    const name = place.text
-    const latitude = place.center[1]
-    const longitude = place.center[0]
-    //console.log(place)
-    console.log(name)
-    console.log(`Latitude = ${latitude}, Longitude = ${longitude}`)
+    if (error) {
+        console.log(chalk.bgRed('Could not connect with the server'))
+
+    } else if (response.body.message) {
+        console.log(chalk.bgYellow(response.body.message))
+
+    } else if (response.body.features.length === 0) {
+        console.log(chalk.bgYellow('Unable to find location'))
+    } else {
+        //console.log(response.body)
+        const place = response.body.features[0]
+        const name = place.text
+        const latitude = place.center[1]
+        const longitude = place.center[0]
+        //console.log(place)
+        console.log(name)
+        console.log(`Latitude = ${latitude}, Longitude = ${longitude}`)
+    }
 })
