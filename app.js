1493ddd9950fcf587e283465bfa33bfa4c5a4339
José Cuevas <josecueram@gmail.com>
2019-03-15 20:22:48 -0600

    Polished some details

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/b570cea920d6af533ac24dbe957983e3/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/b570cea920d6af533ac24dbe957983e3/37.8267,-122.4233?units=si'
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({url: url, json: true}, (error, response) => {
+    const currentWeather = response.body.currently
+    //console.log(currentWeather)
+    console.log(response.body.daily.data[0].summary)
+    console.log(`It is currently ${currentWeather.temperature} degrees outside. There's a ${currentWeather.precipProbability}% chance of rain `)
 })
