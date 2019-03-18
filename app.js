30972a1d33538e8cb2b62ed9da32f94dec97576a
José Cuevas <josecueram@gmail.com>
2019-03-17 19:42:41 -0600

    Moved forecast functionality to utils

--- app.js
+++ app.js
@@ -2,28 +2,7 @@ const request = require('request')
 const chalk = require('chalk')
 
 const geocode = require('./utils/geocode')
-
-
-const forecast = (latitude, longitude, units, callback) => {
-    const url = `https://api.darksky.net/forecast/b570cea920d6af533ac24dbe957983e3/${latitude},${longitude}?units=${units}`
-    
-    request({url: url, json: true}, (error, response) => {
-        if (error) {
-            callback('Could not connect with the server!', undefined)
-        } else if (response.body.error) {
-            callback(response.body.error, undefined)
-        } else {
-            const currentWeather = response.body.currently
-
-            const data = {
-                summary: currentWeather.summary,
-                temperature: currentWeather.temperature
-            }
-
-            callback(undefined, data)
-        }
-    })
-}
+const forecast = require('./utils/forecast')
 
 
 geocode('Xalapa', (error, data) => {
@@ -39,6 +18,6 @@ forecast(19.54, -96.9275, 'si', (error, data) => {
     if (error) {
         console.log(chalk.bgRed(error))
     } else {
-        console.log(chalk.bgGreen(`${data.summary}, current temperature: ${data.temperature}c`))
+        console.log(chalk.bgGreen(`${data.summary}, current temperature: ${data.temperature} ºC`))
     }
 })
---
+++ utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, units, callback) => {
+    const url = `https://api.darksky.net/forecast/b570cea920d6af533ac24dbe957983e3/${latitude},${longitude}?units=${units}`
+    
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Could not connect with the server!', undefined)
+        } else if (response.body.error) {
+            callback(response.body.error, undefined)
+        } else {
+            const currentWeather = response.body.currently
+
+            const data = {
+                summary: currentWeather.summary,
+                temperature: currentWeather.temperature
+            }
+
+            callback(undefined, data)
+        }
+    })
+}
+
+module.exports = forecast
